0ffd70a7e02850ecd011efbded2e89988916f1ac
C03J10 <cte_220000001593@uic.edu.ph>
2024-03-12 03:31:53 +0000

    finish mps

--- mp2.asm
+++ mp2.asm
@@ -3,34 +3,56 @@ section .text
 
 _start:
 
-    ; Print 'T' character 
     mov   eax, 4      
     mov   ebx, 1      
 
-    push  'T'
+    push  '['
     mov   ecx, esp     
     mov   edx, 1      
     int   80h          
+
+    add   esp, 4     
+
+    mov   eax, 4      
+    mov   ebx, 1      
+    
+    push  46
+    mov   ecx, esp    
+    mov   edx, 1      
+    int   80h
+
+    mov   eax, 4      
+    mov   ebx, 1      
+    
+    push  60
+    mov   ecx, esp    
+    mov   edx, 1      
+    int   80h  
+
+    mov   eax, 4      
+    mov   ebx, 1      
+    
+    push  46
+    mov   ecx, esp    
+    mov   edx, 1      
+    int   80h
+
+    mov   eax, 4      
+    mov   ebx, 1      
+    
+    push  ']'
+    mov   ecx, esp    
+    mov   edx, 1      
+    int   80h  
 
     ; Print new line 
     mov   eax, 4     
     mov   ebx, 1       
 
-    push  0x0A
+    push  10
     mov   ecx, esp    
     mov   edx, 1      
-    int   80h         
-
-    add   esp, 4      
-
-    ; Print 'E' character
-    mov   eax, 4      
-    mov   ebx, 1      
-    
-    push  'E'
-    mov   ecx, esp    
-    mov   edx, 1      
-    int   80h         
+    int   80h     
 
     ; return value in EAX = 1 (byte written), or error (-errno)
 
