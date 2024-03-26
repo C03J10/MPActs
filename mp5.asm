51c84292714aa1a1e7dd3950c12bdba692c9de1c
C03J10 <cte_220000001593@uic.edu.ph>
2024-03-26 03:13:41 +0000

    remove comments on mp5

--- mp5.asm
+++ mp5.asm
@@ -1,5 +1,5 @@
 section .data
-    newline db 10        ; newline character
+    newline db 10        
 
 section .bss
     buffer resb 1
@@ -8,29 +8,29 @@ section .text
     global _start
 
 _start:
-    ; Read a character from stdin
-    mov eax, 3           ; sys_read system call number
-    mov ebx, 0           ; file descriptor: stdin
-    mov ecx, buffer      ; buffer to read into
-    mov edx, 1           ; number of bytes to read
-    int 0x80             ; invoke system call
+    
+    mov eax, 3           
+    mov ebx, 0           
+    mov ecx, buffer      
+    mov edx, 1           
+    int 0x80             
 
     cmp byte[buffer], 65
-    jge check_uppercase ; check decimals 65 and above 
-    jl display_symbol   ; display 64 and below
+    jge check_uppercase  
+    jl display_symbol   
 
     check_uppercase: 
         cmp byte[buffer], 91
-        jge check_lowercase ; check 91 and above
-        jl  convert_to_lowercase    ;convert 90 and below
+        jge check_lowercase 
+        jl  convert_to_lowercase    
 
     check_lowercase:
         cmp byte[buffer], 97
-        jl display_symbol ; display 96 and below
+        jl display_symbol 
 
         cmp byte[buffer], 123
-        jl convert_to_uppercase ;convert 122 and below 
-        jge display_symbol ; display 123 and above
+        jl convert_to_uppercase 
+        jge display_symbol 
 
     display_symbol:
         mov eax, 4          
@@ -59,14 +59,13 @@ _start:
         jmp exit_program
     
     exit_program:
-        ; Display a new line
-        mov eax, 4           ; sys_write system call number
-        mov ebx, 1           ; file descriptor: stdout
-        mov ecx, newline     ; pointer to the newline character
-        mov edx, 1           ; length of the newline character
-        int 0x80             ; invoke system call
+        
+        mov eax, 4           
+        mov ebx, 1           
+        mov ecx, newline     
+        mov edx, 1           
+        int 0x80             
 
-        ; Exit the program
-        mov eax, 1           ; sys_exit system call number
-        xor ebx, ebx         ; exit code 0
-        int 0x80             ; invoke system call
+        mov eax, 1           
+        xor ebx, ebx         
+        int 0x80             
